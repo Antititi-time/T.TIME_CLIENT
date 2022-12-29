ec57c59c3be8d3d38d22ba7f724e93900c18f342
choichoijin <cuj10594@yonsei.ac.kr>
2022-12-29 16:24:20 +0900

    [init] recoil, react-query 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 import GlobalStyle from '@src/styles/globalStyle';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
