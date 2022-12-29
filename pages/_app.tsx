9010b822e0274a3b66bda2224e5dfe6e18ec56d5
choichoijin <cuj10594@yonsei.ac.kr>
2022-12-29 16:21:32 +0900

    [init] styled-components 관련 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import GlobalStyle from '@src/styles/globalStyle';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+  }
+  
+  #root {
+    margin: 0 auto;
+  }
+  
+  html {
+    font-size: 62.5%;
+  }
+  
+  * {
+    box-sizing: border-box;
+  }
+  
+  body, button {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+    Helvetica Neue, sans-serif;
+  }
+  
+  button {
+    cursor: pointer;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    -webkit-tap-highlight-color : transparent;
+  }
+  
+  a, a:visited {
+    text-decoration: none;
+    color: black;
+  }
+`;
+
+export default GlobalStyle;
