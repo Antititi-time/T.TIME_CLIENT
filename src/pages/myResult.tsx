e10c191bf8c5fb22fe02a60379a34352278babcc
hayoun <fishingest@kookmin.ac.kr>
2023-01-05 19:21:45 +0900

    [feat]myResult Container

--- src/pages/myResult.tsx
+++ src/pages/myResult.tsx
@@ -1,5 +1,34 @@
+import LogoTop from "@src/components/common/LogoTop";
+import styled from "styled-components";
 function MyResult(){
 
-    return <></>
+    return <>
+    <LogoTop/>
+    <StMyResult>
+        <StWarningMessage></StWarningMessage>
+        <StResultCard>
+            <StInfoContainer></StInfoContainer>
+            <StUserImage/>
+            <StResultTitle></StResultTitle>
+            <StResultDetail></StResultDetail>
+            <StRecommendText></StRecommendText>
+            <StGraphTitle></StGraphTitle>
+            <StGraphContainer></StGraphContainer>
+            <StCardFooter></StCardFooter>
+        </StResultCard>
+    </StMyResult>
+    </>
 }
 export default MyResult;
+
+const StMyResult=styled.div``;
+const StWarningMessage=styled.header``;
+const StResultCard=styled.main``;
+const StInfoContainer=styled.div``;
+const StUserImage=styled.img``;
+const StResultTitle=styled.div``;
+const StResultDetail=styled.div``;
+const StRecommendText=styled.article``;
+const StGraphTitle=styled.div``;
+const StGraphContainer=styled.article``;
+const StCardFooter=styled.footer``;
