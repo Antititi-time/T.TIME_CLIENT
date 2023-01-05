1d71428b7eff62b9de15060494884b836f5fad9b
Jang Myungji <myeongji2105@naver.com>
2023-01-05 16:55:01 +0900

    [feat] 공통 버튼 컴포넌트 생성

---
+++ src/components/common/BottonButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type BottonButtonProps = {
+  width: number;
+  color: string;
+  text: string;
+};
+
+function BottonButton({ width, color, text }: BottonButtonProps) {
+  return <div></div>;
+}
+
+export default BottonButton;
