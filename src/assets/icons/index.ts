e22f7e7faff40ae356507cb8ddd4e4398cf6ce66
hayoun <fishingest@kookmin.ac.kr>
2023-01-05 20:12:00 +0900

    [chore]파일 구조 수정

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,1 @@
-export { default as icPaste } from './icn_paste.svg';
-export { default as icKakao } from './icn_kakao.svg';
+export { default as logoIcon } from '../icons/logoIcon.svg';
--- src/pages/invite.tsx
+++
@@ -1,5 +0,0 @@
-import InviteModal from '@src/components/inviteModal/InviteModal';
-
-export default function Invite() {
-  return <InviteModal></InviteModal>;
-}
