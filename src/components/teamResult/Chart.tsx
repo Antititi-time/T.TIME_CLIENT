986c3d4bdf88a36a0c0253e6c1e6c2ae9930d44c
choichoijin <cuj10594@yonsei.ac.kr>
2023-01-07 01:40:49 +0900

    [design] chart.js 설치 및 기본 틀 불러오기

--- src/components/teamResult/Chart.tsx
+++ src/components/teamResult/Chart.tsx
@@ -2,15 +2,59 @@ import styled from 'styled-components';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 import { TeamScore } from '@src/mocks/types';
+import { Radar } from 'react-chartjs-2';
+import { Chart as ChartJS, LineElement, PointElement, Tooltip, Legend, RadialLinearScale } from 'chart.js/auto';
+import { COLOR } from '@src/styles/color';
+
+ChartJS.register(LineElement, PointElement, Tooltip, Legend, RadialLinearScale);
 
 function Chart() {
   const { data } = useQuery('favorite', () => axios.post('/api/result/team/score/teamId'));
-  console.log(data?.data[0].data);
+  const chartData = {
+    labels: ['협업', '성장', '동기부여', '개인생활', '건강'],
+    datasets: [
+      {
+        label: '팀 점수',
+        data: [62, 68, 68, 67, 67],
+        backgroundColor: 'rgba(255, 108, 61, 0.2)',
+      },
+    ],
+  };
+  const options = {
+    elements: {
+      line: {
+        borderWidth: 2,
+        borderColor: COLOR.ORANGE_1,
+      },
+      point: {
+        pointBackgroundColor: COLOR.ORANGE_1,
+      },
+    },
+    // scales: {
+    //   ticks: {
+    //     min: 0,
+    //     max: 100,
+    //     stepSize: 25,
+    //   },
+    //   angleLines: {
+    //     display: false,
+    //   },
+    // },
+  };
+  const Chart = () => {
+    return (
+      <StRadar>
+        <Radar data={chartData} options={options} />
+      </StRadar>
+    );
+  };
+
   return (
     <StChart>
       {data?.data[0].data.map((data: TeamScore) => (
         <div key={data.questionType}>{data.grade}</div>
       ))}
+      <Chart />
     </StChart>
   );
 }
@@ -19,4 +63,9 @@ export default Chart;
 
 const StChart = styled.div`
   height: 44.2rem;
+`;
+
+const StRadar = styled.div`
+  width: 30.2rem;
+  height: 27.8rem;
 `;
