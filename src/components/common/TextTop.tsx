4858a705956ffdd99a7dd9ab59b38461d9a9b046
seoyeun0106 <sarah.mun16@gmail.com>
2023-01-07 23:59:54 +0900

    [feat] 공통 컴포넌트 생성

---
+++ src/components/common/TextTop.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { FONT_STYLES } from '@src/styles/fontStyle';
+import { COLOR } from '@src/styles/color';
+
+interface TextTopProps {
+  text: string;
+}
+
+function TextTop({ text }: TextTopProps) {
+  return <StJoinTop>{text}</StJoinTop>;
+}
+
+export default TextTop;
+
+const StJoinTop = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  flex-direction: column;
+  min-width: 39rem;
+  width: 100vw;
+  height: 5.8rem;
+  background: rgba(255, 255, 255, 0.8);
+  backdrop-filter: blur(0.5rem);
+  color: ${COLOR.BLACK};
+  ${FONT_STYLES.PRETENDARD_B_20}
+`;
