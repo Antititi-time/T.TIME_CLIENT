0b5eae95427878a7460234ffe3f1ed355a2595e2
choichoijin <cuj10594@yonsei.ac.kr>
2023-01-08 00:31:17 +0900

    [feat] 실 api 연결 코드 작성

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,8 @@ import { api } from './base';
 export const test = async (body: object) => {
   await api.post({ url: `/api/team/729262811`, data: body });
 };
+
+export const getTeamResult = async (teamId: number) => {
+  const { data } = await api.get({ url: `/api/result/team/${teamId}` });
+  return { data };
+};
