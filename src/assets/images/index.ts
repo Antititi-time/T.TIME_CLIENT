7cea84fb05b0175b1f20f833c6ca14580188372f
Na Rim <skfla07@naver.com>
2023-01-10 00:09:30 +0900

    [feat] 404페이지 제작

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -6,3 +6,4 @@ export { default as imgEclair } from './img_eclair.png';
 export { default as imgMakarong } from './img_makarong.png';
 export { default as imgSaltbread } from './img_saltbread.png';
 export { default as imgTart } from './img_tart.png';
+export { default as imgErrorChr } from './img_error_chr.png';
