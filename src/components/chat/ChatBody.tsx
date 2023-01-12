bf597ae43413aca6dce90b9c8d989183f9bb917d
hayoun <fishingest@kookmin.ac.kr>
2023-01-12 16:28:39 +0900

    [chore] Sticky 세팅 수정

--- src/components/chat/ChatBody.tsx
+++ src/components/chat/ChatBody.tsx
@@ -111,7 +111,8 @@ const StEmoticonWrapper = styled.div``;
 const StChatBody = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 8.2rem;
+  padding-bottom: 2rem;
+  margin-top: 1.1rem;
   white-space: pre-line;
   z-index: 0;
 
@@ -146,6 +147,7 @@ const StInputQuestion = styled.div`
   border-radius: 1rem;
   background-color: ${COLOR.BLUE_2};
   color: ${COLOR.BLACK};
+
   ${FONT_STYLES.NEXON_R_13};
   :after {
     position: absolute;
