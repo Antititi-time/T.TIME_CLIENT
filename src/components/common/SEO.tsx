e50f7f8c2a63f3b880466926b7a86413e4224412
choichoijin <cuj10594@yonsei.ac.kr>
2023-01-12 14:49:45 +0900

    [chore] og:url 고정

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -1,12 +1,10 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 
 interface SEOProps {
   title: string;
 }
 
 function SEO({ title }: SEOProps) {
-  const router = useRouter();
   return (
     <Head>
       <title>{title}</title>
@@ -15,7 +13,7 @@ function SEO({ title }: SEOProps) {
       <meta property="og:title" content="T.time" />
       <meta property="og:description" content="T.time | 팀과 내가 함께 성장하는 시간" />
       <meta property="og:image" content="/img_thumbnail.png" />
-      <meta property="og:url" content={`https://t-time-client.vercel.app/${router.asPath}`} />
+      <meta property="og:url" content="https://t-time-client.vercel.app" />
       <meta property="og:locale" content="ko_KR" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" id="viewportMeta" />
     </Head>
