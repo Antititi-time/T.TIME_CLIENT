1dd8e18f945e59a9321ffb2b493f45b1880cc2dc
hayoun <fishingest@kookmin.ac.kr>
2023-01-13 14:00:02 +0900

    [chore] 프리티어 세팅

--- src/components/myResult/ImgDownload.ts
+++ src/components/myResult/ImgDownload.ts
@@ -4,18 +4,17 @@ interface paramsType {
 }
 
 const onDownload = async ({ id, name }: paramsType) => {
-  const DownloadCompo = ( document.getElementById(id)) as HTMLElement;
-  const lib = await import('html2canvas'); 
-  const canvas = await lib.default(DownloadCompo)
+  const DownloadCompo = document.getElementById(id) as HTMLElement;
+  const lib = await import('html2canvas');
+  const canvas = await lib.default(DownloadCompo);
   document.body.appendChild(canvas);
-  onSave(canvas.toDataURL('img/'),name);
-  document.body.removeChild(canvas)
-   
-}
+  onSave(canvas.toDataURL('img/'), name);
+  document.body.removeChild(canvas);
+};
 
 const onSave: (url: string, name: string) => void = (url, name) => {
   const link = document.createElement('a');
-  link.href =  url;
+  link.href = url;
   link.download = name;
   document.body.appendChild(link);
   link.click();
