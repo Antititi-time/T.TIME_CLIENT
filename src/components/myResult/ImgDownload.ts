f6c27d565e86a63f09bcf83ea05393b74baaeb1a
seoyeun0106 <sarah.mun16@gmail.com>
2023-01-13 09:43:48 +0900

    [fix] 다이나믹 import 로 라이브러리 사용

--- src/components/myResult/ImgDownload.ts
+++ src/components/myResult/ImgDownload.ts
@@ -1,24 +1,21 @@
-import html2canvas from 'html2canvas';
-
 interface paramsType {
   id: string;
   name: string;
 }
 
 const onDownload = async ({ id, name }: paramsType) => {
-  if (typeof window !== 'object') return;
-  const DownloadCompo = (await document.getElementById(id)) as HTMLElement;
-  if (DownloadCompo) {
-    const canvas = await html2canvas(DownloadCompo, { allowTaint: true, useCORS: true });
-    document.body.appendChild(canvas);
-    onSave(canvas.toDataURL('myResultImg'), name);
-    document.body.removeChild(canvas);
-  }
-};
+  const DownloadCompo = ( document.getElementById(id)) as HTMLElement;
+  const lib = await import('html2canvas'); 
+  const canvas = await lib.default(DownloadCompo)
+  document.body.appendChild(canvas);
+  onSave(canvas.toDataURL('img/'),name);
+  document.body.removeChild(canvas)
+   
+}
 
 const onSave: (url: string, name: string) => void = (url, name) => {
   const link = document.createElement('a');
-  link.href = url;
+  link.href =  url;
   link.download = name;
   document.body.appendChild(link);
   link.click();
