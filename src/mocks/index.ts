4a66cf697425564ad8bda2e7de82df0714afdf44
choichoijin <cuj10594@yonsei.ac.kr>
2023-01-13 21:25:49 +0900

    [chore] msw 주석 처리

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,9 +1,9 @@
-if (typeof window === 'undefined') {
-  const server = import('./server');
-  server.then((s) => s.server.listen());
-} else {
-  const worker = import('./browser');
-  worker.then((w) => w.worker.start());
-}
+// if (typeof window === 'undefined') {
+//   const server = import('./server');
+//   server.then((s) => s.server.listen());
+// } else {
+//   const worker = import('./browser');
+//   worker.then((w) => w.worker.start());
+// }
 
 export {};
