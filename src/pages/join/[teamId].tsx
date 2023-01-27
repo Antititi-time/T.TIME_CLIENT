864550a2934b270f7e98414eed5a277a71de8ac3
Na Rim <skfla07@naver.com>
2023-01-27 15:29:12 +0900

    [chore] 화살표 함수 변경

--- src/pages/join/[teamId].tsx
+++ src/pages/join/[teamId].tsx
@@ -101,7 +101,7 @@ function Join() {
           value={nickname || ''}></StInputBox>
         <StNotice>4글자 이내 한글로 입력해주세요</StNotice>
       </StInputContainer>
-      <StButtonContainer onClick={() => handleSubmit()}>
+      <StButtonContainer onClick={handleSubmit}>
         <BottomButton width={28.2} color={COLOR.ORANGE_1} text={'다음'} />
       </StButtonContainer>
     </StJoin>
