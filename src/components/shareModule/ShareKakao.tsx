5511aa4f28789b9df0bc236ad5ea54b7aaec0a48
hayoun <fishingest@kookmin.ac.kr>
2023-01-31 01:44:25 +0900

    [feat] 카카오 팀 결과 공유 이미지 추가

--- src/components/shareModule/ShareKakao.tsx
+++ src/components/shareModule/ShareKakao.tsx
@@ -36,7 +36,8 @@ export const shareKakao = (route: string, name: string, shareType: string) => {
           title: `${name}팀의 티타임 결과를 확인하세요.`,
           description: '팀 결과를 통해 팀원들의 속마음을 확인해보세요.☕️',
 
-          imageUrl: process.env.NEXT_PUBLIC_KAKAO_SHARE_IMAGE,
+          imageUrl:
+            'https://github.com/Antititi-time/T.TIME_CLIENT/blob/feat/%23102/src/assets/images/img_kakao_teamShare.jpg?raw=true',
           link: {
             mobileWebUrl: route,
             webUrl: route,
@@ -60,7 +61,7 @@ export const shareKakao = (route: string, name: string, shareType: string) => {
           title: `${name}님의 티타임 결과를 확인하세요.`,
           description: '결과를 통해 나의 상태를 확인해보세요.☕️',
           imageUrl:
-            'https://github.com/Antititi-time/T.TIME_CLIENT/blob/feat/%23102/src/assets/images/kakao_Personal.jpg?raw=true',
+            'https://github.com/Antititi-time/T.TIME_CLIENT/blob/feat/%23102/src/assets/images/img_kakao_personalShare.jpg?raw=true',
           link: {
             mobileWebUrl: route,
             webUrl: route,
