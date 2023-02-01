9a05440d7fc1a3f879121afdf98ab8ae191a0934
hayoun <fishingest@kookmin.ac.kr>
2023-02-01 16:57:47 +0900

    [feat] Unfinished Result Day 로직 추가

--- src/components/teamResult/UnfinishedResult.tsx
+++ src/components/teamResult/UnfinishedResult.tsx
@@ -24,9 +24,12 @@ function UnfinishedResult({ completeData }: completeDataType) {
   const date = new Date();
   const year = date.getFullYear();
   let month: string | number = date.getMonth() + 1;
-  const day: string | number = date.getDate();
+  let day: string | number = date.getDate();
   if (month < 10) {
     month = '0' + month;
+  }
+  if (day < 10) {
+    day = '0' + day;
   }
 
   return (
