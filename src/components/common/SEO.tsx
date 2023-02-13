5feff7bbced48f3722c129598eaa165d66037496
choichoijin <cuj10594@yonsei.ac.kr>
2023-02-13 16:51:20 +0900

    [chore] SEO 컴포넌트 프롭스 수정

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -2,20 +2,23 @@ import Head from 'next/head';
 
 interface SEOProps {
   title: string;
+  ogTitle?: string;
   description: string;
+  image?: string;
+  url?: string;
 }
 
-function SEO({ title, description }: SEOProps) {
+function SEO({ title, ogTitle, description, image, url }: SEOProps) {
   return (
     <Head>
       <title>{title}</title>
       <link rel="icon" href="/favicon.ico" />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="T.time" />
-      <meta property="og:title" content="T.time" />
+      <meta property="og:title" content={ogTitle ?? 'T.time'} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content="/img_thumbnail.png" />
-      <meta property="og:url" content="https://t-time.vercel.app/" />
+      <meta property="og:image" content={image ?? '/img_thumbnail.png'} />
+      <meta property="og:url" content={url ?? '/img_thumbnail.png'} />
       <meta property="og:locale" content="ko_KR" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" id="viewportMeta" />
     </Head>
