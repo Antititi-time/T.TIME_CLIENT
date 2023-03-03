ab8b23c8d31c2ea5ad4ab68a0536502c2961cacc
Jang Myungji <myeongji2105@naver.com>
2023-03-03 14:29:37 +0900

    [feat] 버튼 고정 슬라이드

--- src/pages/organizerOnboarding.tsx
+++ src/pages/organizerOnboarding.tsx
@@ -1,7 +1,7 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
 import { COLOR } from '@src/styles/color';
 import { imgGoogleIcon, imgKakaoIcon } from '@src/assets/images';
@@ -12,6 +12,7 @@ import { ORGANIZER_SLIDER_ITEMS } from '@src/constants/organizerOnboarding/organ
 
 function OrganizerOnboarding() {
   const sliderRef = useRef<Slider>(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
   const settings = {
     dots: true,
     dotsClass: 'customDots',
@@ -24,48 +25,67 @@ function OrganizerOnboarding() {
     sliderRef.current?.slickGoTo(4);
   };
   return (
-    <StSlider {...settings} ref={sliderRef}>
-      {ORGANIZER_SLIDER_ITEMS.map((item, idx) => (
-        <StOnboardingWrapper key={idx}>
-          <StFlex>
-            <PageText>{ORGANIZER_SLIDER_ITEMS[item.id - 1].text}</PageText>
-            <ImageDiv
-              src={ORGANIZER_SLIDER_ITEMS[item.id - 1].image}
-              alt="주최자 온보딩 이모티콘"
-              className="organizerEmoticon"
-              fill={true}
-            />
-            {item.id < 5 ? (
-              <div onClick={on}>
-                <BottomButton width={28.2} color={COLOR.BLUE_1} text={'건너뛰기'} />
-              </div>
-            ) : (
-              <StSocialLoginButton>
-                <StInfor>SNS 계정으로 티타임을 편리하게 이용해 보세요.</StInfor>
-                <GoogleLoginButton>
-                  <ImageDiv src={imgGoogleIcon} alt="구글 아이콘" className="googleIcon" fill={true} />
-                  <GoogleLoginText>Google 계정으로 시작하기</GoogleLoginText>
-                </GoogleLoginButton>
-                <KakaoLoginButton>
-                  <ImageDiv src={imgKakaoIcon} alt="카카오 아이콘" className="kakaoIcon" fill={true} />
-                  <KakaoLoginText>카카오 계정으로 시작하기</KakaoLoginText>
-                </KakaoLoginButton>
-              </StSocialLoginButton>
-            )}
-          </StFlex>
-        </StOnboardingWrapper>
-      ))}
-    </StSlider>
+    <>
+      <StSlider
+        {...settings}
+        ref={sliderRef}
+        afterChange={(newSlide) => {
+          setCurrentSlide(newSlide);
+        }}>
+        {ORGANIZER_SLIDER_ITEMS.map((item, idx) => (
+          <StOnboardingWrapper key={idx}>
+            <StFlex>
+              <PageText>{ORGANIZER_SLIDER_ITEMS[item.id - 1].text}</PageText>
+              <ImageDiv
+                src={ORGANIZER_SLIDER_ITEMS[item.id - 1].image}
+                alt="주최자 온보딩 이모티콘"
+                className="organizerEmoticon"
+                fill={true}
+              />
+            </StFlex>
+          </StOnboardingWrapper>
+        ))}
+      </StSlider>
+      {currentSlide < 4 ? (
+        <StSkipButton>
+          <div onClick={on}>
+            <BottomButton width={28.2} color={COLOR.BLUE_1} text={'건너뛰기'} />
+          </div>
+        </StSkipButton>
+      ) : (
+        <StSocialLoginButton>
+          <StInfor>SNS 계정으로 티타임을 편리하게 이용해 보세요.</StInfor>
+          <GoogleLoginButton>
+            <ImageDiv src={imgGoogleIcon} alt="구글 아이콘" className="googleIcon" fill={true} />
+            <GoogleLoginText>Google 계정으로 시작하기</GoogleLoginText>
+          </GoogleLoginButton>
+          <KakaoLoginButton>
+            <ImageDiv src={imgKakaoIcon} alt="카카오 아이콘" className="kakaoIcon" fill={true} />
+            <KakaoLoginText>카카오 계정으로 시작하기</KakaoLoginText>
+          </KakaoLoginButton>
+        </StSocialLoginButton>
+      )}
+    </>
   );
 }
 
 export default OrganizerOnboarding;
+
+const StSkipButton = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: 18.5rem;
+  margin-left: -14.1rem;
+`;
 
 const StSocialLoginButton = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  position: relative;
+  top: -29.2rem;
 `;
 
 const StSlider = styled(Slider)`
@@ -99,9 +119,9 @@ const StSlider = styled(Slider)`
   .customDots li {
     position: relative;
     display: inline-block;
-    width: 20px;
-    height: 20px;
-    margin: 0 5px;
+    width: 2rem;
+    height: 2rem;
+    margin: 0 0.5rem;
     padding: 0;
     cursor: pointer;
   }
@@ -109,9 +129,9 @@ const StSlider = styled(Slider)`
     font-size: 0;
     line-height: 0;
     display: block;
-    width: 10px;
-    height: 10px;
-    padding: 3px;
+    width: 1rem;
+    height: 1rem;
+    padding: 0.3rem;
     cursor: pointer;
     color: transparent;
     border: 0;
@@ -120,8 +140,8 @@ const StSlider = styled(Slider)`
   }
   .customDots li button:before {
     font-family: 'slick';
-    font-size: 6px;
-    line-height: 20px;
+    font-size: 0.6rem;
+    line-height: 2rem;
     position: absolute;
     top: 0;
     left: 0;
@@ -148,6 +168,7 @@ const StSlider = styled(Slider)`
 `;
 
 const PageText = styled.p`
+  position: relative;
   margin-top: 7.2rem;
   text-align: center;
   ${FONT_STYLES.PRETENDARD_B_16};
@@ -161,6 +182,8 @@ const StFlex = styled.div`
   align-content: center;
   justify-content: center;
   align-items: center;
+  position: relative;
+  top: -7.2rem;
 `;
 
 const StOnboardingWrapper = styled.div`
