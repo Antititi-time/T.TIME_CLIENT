5ad452f096069c9ea3bae9de2f80500d698f6c44
Na Rim <skfla07@naver.com>
2023-03-06 00:00:00 +0900

    [chore] 토큰유효성 검사 조건 추가

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -9,20 +9,23 @@ interface withLoginCheckerProps {
 
 function LoginChecker({ children }: withLoginCheckerProps) {
   const Router = useRouter();
+  const { pathname } = useRouter();
   const token = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null;
   useQuery(['isValid', token], () => getTokenValidation(token), {
     enabled: !!token,
     retry: false,
     onError: () => {
       // 토큰 형태가 올바르지 않을 경우
-      localStorage.removeItem('accessToken');
-      localStorage.removeItem('nickName');
-      Router.push('/organizerOnboarding');
-      alert('잘못된 접근입니다. 다시 로그인해주세요');
+      if (pathname !== '/myResult/*' && pathname !== '/teamResult/*') {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('nickName');
+        Router.push('/organizerOnboarding');
+        alert('잘못된 접근입니다. 다시 로그인해주세요');
+      }
     },
     onSuccess: (res) => {
       // 만료된 토큰일 경우
-      if (res.isvalid === false) {
+      if (pathname !== '/myResult/*' && pathname !== '/teamResult/*' && res.isvalid === false) {
         localStorage.removeItem('accessToken');
         localStorage.removeItem('nickName');
         Router.push('/organizerOnboarding');
