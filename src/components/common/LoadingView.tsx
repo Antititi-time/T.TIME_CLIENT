996e5f7a748db1446edc9492f12390f0f725280d
최유진 <cuj10594@naver.com>
2023-06-08 21:56:09 +0900

    [design] 로딩중 컴포넌트 반응형 구현

--- src/components/common/LoadingView.tsx
+++ src/components/common/LoadingView.tsx
@@ -17,14 +17,35 @@ const StLoadingView = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 50rem;
   touch-action: none;
+  @media screen and (max-width: 765px) {
+    margin-top: 17.9rem;
+  }
+
+  @media screen and (min-width: 766px) {
+    margin-top: 22.1rem;
+  }
+
   img {
-    width: 20rem;
-    height: 20rem;
+    @media screen and (max-width: 765px) {
+      width: 20rem;
+      height: 20rem;
+    }
+
+    @media screen and (min-width: 766px) {
+      width: 32rem;
+      height: 32rem;
+    }
   }
 `;
 const StLoadingText = styled.p`
   color: ${COLOR.BLUE_TEXT};
-  ${FONT_STYLES.NEXON_B_20};
+
+  @media screen and (max-width: 765px) {
+    ${FONT_STYLES.NEXON_B_20};
+  }
+
+  @media screen and (min-width: 766px) {
+    ${FONT_STYLES.NEXON_B_24};
+  }
 `;
